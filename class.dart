2662c1c9d6e34b4ff28f26f714fb1b36235dd327
farhanmahmud21 <mahmudkhanfarhan2001@gmail.com>
2025-01-16 03:10:31 +0600

    overall delete and show status updated

--- class.dart
+++ class.dart
@@ -7,20 +7,42 @@ class Task{
 
   Task({required this.id,required this.title,required this.description,required this.status,required this.dueDate});
 }
+
 class TaskManager{
   List<Task> taskList=[];
+
   addNewTask(String title,String description,String status,DateTime dueDate){
     int id= taskList.length+1;
     taskList.add(Task(id: id, title: title, description: description, status: status, dueDate: dueDate));
-print('Title added: $title');
-updateTask(int id,String title,String status){
-if(taskList[id]==id){
-  status='done';
-}
-}
+    print('Title added: $title');
+  }
+
+  updateTask(int id,String title,String status){
+    if(taskList[id].id == id){
+      taskList[id].status = 'done';
+    }
+    print('Task updated: $title');
+  }
+
+  deleteTask(int id){
+    if(taskList[id].id == id){
+      taskList.removeAt(id);
+      print('Task deleted: $id');
+    }
+  }
+
+  showTaskStatus(){
+    for(int i=0;i<taskList.length;i++){
+      print('Task: ${taskList[i].title} Status: ${taskList[i].status}');
+    }
   }
 }
+
 void main(){
   TaskManager taskManager=TaskManager();
   taskManager.addNewTask( 'Farhan', "Farhan", 'Pending', DateTime.now());
+  taskManager.addNewTask( 'Antu', "Farhan", 'Pending', DateTime.now());
+  taskManager.updateTask(1, 'Farhan', 'done');
+  taskManager.deleteTask(1);
+  taskManager.showTaskStatus();
 }
